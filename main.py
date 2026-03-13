213592f1a783c87b7d87d937385bc889bec34bf9
michaelc994 <ichenman@gmail.com>
2026-03-13 12:01:03 -0400

    used groups

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+
 
     while True:
         log_state()
@@ -20,10 +27,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+            
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
