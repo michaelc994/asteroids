7b8d8556ff3e49413377fa5b3b76a65eb89fc0a2
michaelc994 <ichenman@gmail.com>
2026-03-13 11:45:25 -0400

    add player rotation

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from logger import log_state
 from player import Player
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
-    pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         log_state()
@@ -22,13 +20,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
+        player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-        clock.tick(60)
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -1,21 +1,12 @@
 import pygame
 
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
-    # in the Player class
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
 
     def draw(self, screen):
         pygame.draw.polygon(
@@ -24,3 +15,22 @@ class Player(CircleShape):
             self.triangle(),
             LINE_WIDTH
         )
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
